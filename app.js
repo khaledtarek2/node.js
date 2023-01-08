b8d03bc1ab1ca91f40a81951418dc75c969ec121
khaledtarek <kingdodo987@gmail.com>
2023-01-08 22:18:36 +0200

    commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// console.log('Welcome to Node Tutorial')
+// const lo = require('lodash');
+// const items = [1, [2, [3, [4]]]]
+// const newItems = lo.flattenDeep(items)
+// console.log(newItems)
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// const sum2 = (a, b) => a + b
+
+
+
+// console.log(sum(1,5))
+// console.log(sum2(1,5))
+
+
+// class Person {
+//     constructor(name){
+//         this.name = name
+//     }
+
+
+// printNameArrow() {
+//     setTimeout(() => {
+//         console.log('Arrow: ' + this.name)
+//     }, 100)
+// }
+
+// printNameFunction(){
+//     setTimeout(function(){
+//        console.log(`function: ` + this.name) 
+//     }, 100)
+// }
+// }
+
+// let person = new Person('Bob')
+// person.printNameArrow()
+// person.printNameFunction()
+// console.log(this.name)
+
+// const isEven = element => element % 2 === 0;
+const result = [2, 4, 6, 8].every((e) => e % 2 === 0);
+console.log(result)
+
